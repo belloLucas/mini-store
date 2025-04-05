382ac5cbf7bc0183c8a52b4b995d01d4d84a80a3
belloLucas <lucasbellodev@gmail.com>
2025-04-05 16:16:33 -0300

    refactor: updating table name

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
+    protected $table = 'products_category';
+
     protected $fillable = [
         'name',
         'description',
