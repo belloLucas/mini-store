03ffabdfc801151c47d0e6a42395b2dd49ae3dfa
belloLucas <lucasbellodev@gmail.com>
2025-04-05 16:06:36 -0300

    feat: creating model for products_category

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
