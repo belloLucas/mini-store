43dda8fade4140ed6e2a201925c54a557539f80c
belloLucas <lucasbellodev@gmail.com>
2025-04-05 16:07:07 -0300

    feat: creating product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image_url',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'updated_at' => 'datetime',
+        'created_at' => 'datetime',
+    ];
+
+    public function productCategory()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
 }
