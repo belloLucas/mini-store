53e137282508ff3ec2e58581914a2fb4486acb29
belloLucas <lucasbellodev@gmail.com>
2025-04-06 13:07:22 -0300

    feat: creating cartStore

---
+++ resources/js/stores/cartStore.js
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+
+export const useCartStore = defineStore("cart", {
+    state: () => ({
+        cart: [],
+        isCartModalOpen: false,
+    }),
+    getters: {
+        totalItems: (state) => state.cart.length,
+        totalPrice: (state) =>
+            state.cart.reduce(
+                (sum, item) => sum + item.price * item.quantity,
+                0
+            ),
+    },
+    actions: {
+        addToCart(product) {
+            const existingItem = this.cart.find(
+                (item) => item.id === product.id
+            );
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                this.cart.push({ ...product, quantity: 1 });
+            }
+        },
+        removeFromCart(productId) {
+            this.cart = this.cart.filter((item) => item.id !== productId);
+        },
+        toggleCartModal() {
+            this.isCartModalOpen = !this.isCartModalOpen;
+        },
+    },
+});
